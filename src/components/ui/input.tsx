871d0a0bf4f9012c322cc9ac4ee9b764024cec5c
Krish Anth <krishanthmaya@gmail.com>
2025-04-26 12:17:02 +0000

    set the form as react hook form

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,7 +12,6 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
-        defaultValue="" // Provide a default value to the input
         {...props}
       />
     )
@@ -21,3 +20,4 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 Input.displayName = "Input"
 
 export { Input }
+
